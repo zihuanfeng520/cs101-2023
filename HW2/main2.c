2759694fe0d17f3e8ff60a10af1ae5980dc4b424
王英瑋 <zihuanfeng520@gmail.com>
2023-03-15 23:53:21 +0800

    Create main2.c

---
+++ HW2/main2.c
@@ -0,0 +1,24 @@
+clude <stdio.h>
+# include <string.h>
+
+int main()
+{
+    FILE* fp;
+    FILE* pfp;
+    char a[1000];
+    int b= 0;
+    
+    fp= fopen("main2.txt", "w");
+    pfp= fopen("main2.c", "r");
+    
+    while (fgets(a+b, 1000-b, pfp))
+    {
+        b= b+strlen(a+b);
+    }
+    
+    fprintf(fp, "%s", a);
+    fclose(fp);
+    fclose(pfp);
+    
+    return 0;
+}
