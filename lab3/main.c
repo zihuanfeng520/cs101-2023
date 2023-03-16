8b4a374861b81523977689419df4f632a29aee9d
王英瑋 <zihuanfeng520@gmail.com>
2023-03-16 15:08:05 +0800

    Update main.c

--- lab3/main.c
+++ lab3/main.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+typedef struct person
+{
+    int id;
+    char first_name[32];
+    char last_name[32];
+}person_t;
+
+
+int main()
+{
+    FILE* fp;
+    fp= fopen("person.bin", "w+");
+    person_t p[5]= {
+        {1, "IU", "Lee"},
+        {2, "IUU", "Lee"},
+        {3, "IUUU", "Lee"},
+        {4, "IUUUU", "Lee"},
+        {5, "IUUUUU", "Lee"},
+    };
+    for (int i= 0; i<5; i++)
+    {
+        fwrite(p+i, sizeof(person_t), 1, fp);
+    }
+    fclose(fp);
+    
+    return 0;
+}
