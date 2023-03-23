196ccd9749cafd37ce22ea03105d8c7553250af6
王英瑋 <zihuanfeng520@gmail.com>
2023-03-23 14:37:09 +0800

    Create main.c

---
+++ lab4/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void replace_all(char* str, char oldChar, char newChar)
+{
+    if (* str==oldChar)
+    {
+        * str= newChar;
+    }
+    str++;
+}
+
+int main()
+{
+    char str[100], oldChar= 'a', newChar= 'A';
+    printf("Entry any string: ");
+    scanf("%s", str);
+    
+    printf("\nString before replacing: \n%s", str);
+    replace_all(str, oldChar, newChar);
+    printf("\n\nString after replacing %c with %c : \n%s", oldChar, newChar, str);
+
+    return 0;
+}
