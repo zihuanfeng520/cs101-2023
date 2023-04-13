cb402fe8abce7a07d99a3af7941818c8ec7d8fe7
王英瑋 <zihuanfeng520@gmail.com>
2023-04-13 14:32:04 +0800

    Update main.c

--- lab6/main.c
+++ lab6/main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
 void multi(int i, int j) {
-    printf("%d*%d=%-2d ", i, j, i*j);
+    printf("%d*%d=%d ", i, j, i*j);
+    if (j == 9) {
+        printf("\n");
+        return;
+    } else {
+        multi(i, j+1);
+    }
+}
+
+void table(int i) {
+    if (i == 10) {
+        return;
+    }
+    multi(i, 1);
+    table(i+1);
 }
 
 int main() {
-    for (int i = 1; i <= 9; i++) {
-        for (int j = 1; j <= 9; j++) {
-            multi(i, j);
-        }
-        printf("\n");
-    }
+    table(1);
     return 0;
 }
