973852632d89bc84933c9d0de3284524ac6516e1
王英瑋 <zihuanfeng520@gmail.com>
2023-06-01 16:56:11 +0800

    Create main.cpp

---
+++ Lab10/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+#include <string>
+
+#include <fstream>
+
+using namespace std;
+
+class myString
+
+{
+
+    private:
+
+    string m_str;
+
+    public:
+
+    myString(string s){
+
+        m_str=s;
+
+    }
+
+};
+
+class ReadClass
+
+{
+
+    public:
+
+    void showClass(){
+
+        int n=0;
+
+        ifstream in;
+
+        string line,str("class");
+
+        in.open("main.cpp");
+
+        if(in.fail()){
+
+            cout<<"Erro to open file."<<endl;
+
+        }
+
+        while (getline(in, line)) {
+
+            if (line.find(str) != string::npos){
+
+                const char* characters = line.c_str();
+
+                if(characters[0]=='c'){
+
+                    cout << characters << endl;
+
+                    n++;
+
+                }
+
+            }
+
+        }
+
+        cout<<n<<" class in main.cpp"<<endl;
+
+    }
+
+};
+
+int main()
+
+{
+
+    ReadClass rfile;
+
+    rfile.showClass();
+
+    return 0;
+
+}
